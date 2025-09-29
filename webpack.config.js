8eb37094b4cad861d6b35298d9ff978e88e12b58
romanlysyk <roma-lysyk@ukr.net>
2025-09-29 18:11:13 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,61 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { SourceMapDevToolPlugin, ProvidePlugin } = require("webpack")
 const path = require("path");
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
 
-let mode = "development";
-
-if (process.env.NODE_ENV === "production") {
-	mode = "production";
-}
+let mode = "production";
 console.log(mode + " mode");
 
 module.exports = {
 	mode: mode,
+	devtool: mode === "development" ? "inline-source-map" : false,
 	output: {
-		filename: "[name].[contenthash].js",
-		assetModuleFilename: "assets/[name][ext][query]",
+		path: path.resolve(__dirname, "dist"),
 		clean: true,
+		publicPath: ""
 	},
-	devtool: "source-map",
 	optimization: {
-		splitChunks: {
-			chunks: "all",
+		splitChunks: false,
+		runtimeChunk: false,
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
 		},
 	},
 	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].[contenthash].css",
+		new HtmlBundlerPlugin({
+			entry: {
+				index: {
+					import: "src/index.html",
+					filename: "index.html",
+				},
+			},
+			js: {
+				inline: true,
+				filename: false,
+				chunk: {
+					vendor: false,
+				},
+			},
+			css: {
+				inline: true,
+			},
+			preload: false,
+			prefetch: false,
 		}),
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-		new SourceMapDevToolPlugin(),
-		new ProvidePlugin({ PIXI: 'pixi.js' })
 	],
 	module: {
 		rules: [
 			{
-				test: /\.html$/i,
-				loader: "html-loader",
+				test: /\.html$/,
+				loader: HtmlBundlerPlugin.loader,
 			},
 			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [
-					mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
-					"css-loader",
-					{
-						loader: "postcss-loader",
-						options: {
-							postcssOptions: {
-								plugins: [
-									[
-										"postcss-preset-env",
-										{
-											// Options
-										},
-									],
-								],
-							},
-						},
-					},
-					"sass-loader",
-				],
+				test: /\.(s?css)$/,
+				use: ["css-loader", "postcss-loader", "sass-loader"],
 			},
 			{
-				test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
+				test: /\.(png|svg|jpe?g|gif|ico|webp|woff|woff2|eot|ttf|otf|mp3|ogg|wav)$/i,
+				type: "asset/inline",
 			},
 			{
 				test: /\.m?js$/,
@@ -79,7 +66,19 @@ module.exports = {
 						presets: ["@babel/preset-env"],
 					},
 				},
+				parser: {
+					javascript: {
+						// Примусово збираємо ВСІ dynamic import() як звичайні (без окремих чанків)
+						dynamicImportMode: "eager",
+					},
+				},
 			},
 		],
+	},
+	// Налаштування для dev-сервера
+	devServer: {
+		static: path.resolve(__dirname, "dist"),
+		watchFiles: ["src/**/*.*"],
+		hot: true,
 	},
 };
